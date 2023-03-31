c5c8becbde6ea9f12f208a036f5887641a988417
kunald-ovyo <129162542+kunald-ovyo@users.noreply.github.com>
2023-03-31 17:52:10 +0530

    html css bacis stuff implemeted

---
+++ react-full-stack/pages/HTML-css/Flexbox.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "@/styles/flexbox.module.css";
+
+function Flexbox() {
+  return (
+    <div className={styles.base}>
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+      <div className={styles.box} />
+    </div>
+  );
+}
+
+export default Flexbox;
