9e785146861bd68fab6aee8aa29540ce8d7fea79
kunald-ovyo <129162542+kunald-ovyo@users.noreply.github.com>
2023-04-05 19:22:01 +0530

    increament assignment

--- react-full-stack/pages/HTML-css/Assignment1.tsx
+++ react-full-stack/pages/HTML-css/Assignment1.tsx
@@ -1,19 +1,33 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useCallback, useEffect, useRef, useState } from "react";
 import style from "@/styles/assignment1.module.css";
+import { setInterval } from "timers";
 
-function Assignment1() {
+const Assignment1 = () => {
+  const [count, setcount] = useState<number>(0);
+  const intervalRef = useRef(undefined);
+
+  useEffect(() => {
+    if (count < 10) {
+      const timeout = setTimeout(() => {
+        setcount(count + 1);
+        clearTimeout(timeout);
+      }, 1000);
+    }
+  }, [count]);
+
   return (
     <div className={style.base}>
+      <h1 className={style.header}>{count}</h1>
       <button
         className={style.box}
         onClick={() => {
-          console.log("Kuna checking button");
+          setcount(count + 1);
         }}
       >
         <h1>Checking</h1>
       </button>
     </div>
   );
-}
+};
 
 export default memo(Assignment1);
