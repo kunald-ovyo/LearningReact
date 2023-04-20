dc45d069b6ebb73993e495c9b90f2aa214f62cbc
kunald-ovyo <129162542+kunald-ovyo@users.noreply.github.com>
2023-04-20 17:46:53 +0530

    added horizontal scrolling assets

--- react-full-stack/pages/HTML-css/AssetDetailPage.tsx
+++ react-full-stack/pages/HTML-css/AssetDetailPage.tsx
@@ -4,22 +4,127 @@ import styles from "@/styles/assetDetailPage.module.css";
 function AssetDetailPage() {
   return (
     <div className={styles.baseAssignment}>
-      <div className={styles.bannerView}>
-        <h1 className={styles.textTitle}>checking</h1>
-        <h1 className={styles.textSubtitle}> this is subtitle</h1>
-        <div className={styles.buttonContainer}>
-          <button>
-            <div className={styles.playButton} />
-          </button>
-          <button>
-            <div className={styles.playButton} />
-          </button>
-          <button>
-            <div className={styles.playButton} />
-          </button>
+      <div className={styles.fullpage}>
+        <div className={styles.bannerView}>
+          <h1 className={styles.textTitle}>checking</h1>
+          <h1 className={styles.textSubtitle}> this is subtitle</h1>
+          <div className={styles.buttonContainer}>
+            <button>
+              <div className={styles.playButton} />
+            </button>
+            <button>
+              <div className={styles.playButton} />
+            </button>
+            <button>
+              <div className={styles.playButton} />
+            </button>
+          </div>
+        </div>
+        <div className={styles.assetsHorrizontalList}>
+          <h1 className={styles.textTitle}>checking</h1>
+          <div className={styles.assetsHorrizontalListone}>
+            <div className={styles.innverContainerView}>
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.assetsHorrizontalList}>
+          <h1 className={styles.textTitle}>checking</h1>
+          <div className={styles.assetsHorrizontalListone}>
+            <div className={styles.innverContainerView}>
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.assetsHorrizontalList}>
+          <h1 className={styles.textTitle}>checking</h1>
+          <div className={styles.assetsHorrizontalListone}>
+            <div className={styles.innverContainerView}>
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.assetsHorrizontalList}>
+          <h1 className={styles.textTitle}>checking</h1>
+          <div className={styles.assetsHorrizontalListone}>
+            <div className={styles.innverContainerView}>
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+              <div className={styles.theAssetImageBox} />
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.bottomView}></div>
     </div>
   );
 }
